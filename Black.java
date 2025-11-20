b3439e9c52d6df4c37f7b7ee053d2fa7050599fa
Attila Juhasz <106558542+attilamjuhasz@users.noreply.github.com>
2025-11-20 15:59:06 +0000

    Fixed, kinda. Some bugs left

--- Black.java
+++ Black.java
@@ -12,7 +12,7 @@ public class Black {
         int rep = 0;
 
         while (true){
-            if (rep > 0){
+            if (rep >= 0){
                 System.out.println("");
                 System.out.println("Play? (y/n)");
                 String playAgain = scanner.nextLine();
@@ -20,126 +20,144 @@ public class Black {
                     break;
                 }
                 else if (playAgain.equals("y")){
+                    System.out.println("Bet: ");
+                    double bet = scanner.nextDouble();
+                    money -= bet;
 
+
+                    int card1 = (int)(Math.random() * 10) + 2;
+                    int card2 = 0;
+                    if (card1 == 11){
+                        card2 = (int)(Math.random() * 9) + 2;
+                    }
+                    else{
+                        card2 = (int)(Math.random() * 10) + 2;
+                    }
+                    int pTotal = card1 + card2;
+
+                    int dealer1 = (int)(Math.random() * 10) + 2;
+                    int dealer2 = 0;
+                    if (dealer1 == 11){
+                        dealer2 = (int)(Math.random() * 10) + 2;
+                    }
+                    else{
+                        dealer2 = (int)(Math.random() * 10) + 2;
+                    }
+                    int dTotal = dealer1 + dealer2;
+
+                    System.out.println("");
+                    System.out.println("Dealer has: " + dealer1);
+                    System.out.println("");
+                    System.out.println("Your cards: " + card1 + " and " + card2);
+
+                    String out = "";
+
+
+                    while (pTotal < 22 && out.isEmpty()){
+                        System.out.println("");
+                        System.out.println("Hit, Stand, or Double Down?");
+                        String choice = scanner.next();
+                        choice = choice.toLowerCase();
+
+                        if (choice.equals("hit")) {
+                            int card = (int)(Math.random() * 10) + 2;
+                            pTotal += card;
+                            System.out.println("");
+                            System.out.println("You were dealt: " + card);
+                            System.out.println("Total: " + pTotal);
+                        }
+                        else if (choice.equals("stand")) {
+                            out = "stand";
+                        }
+                        else if (true) {
+                            out = "stand";
+                            money -= bet;
+                            bet = bet*2;
+                            int card = (int)(Math.random() * 10) + 2;
+                            pTotal += card;
+                            System.out.println("");
+                            System.out.println("You were dealt: " + card);
+                            System.out.println("Total: " + pTotal);
+                        }
+                    }
+
+                    if (pTotal >= 22){
+                        out = "";
+                        System.out.println("");
+                        System.out.println("You bust!");
+                        System.out.println("Money: " + money);
+                        money -= bet;
+                        continue;
+                    }
+
+                    while (dTotal < 17 && pTotal < 22) {
+                        int dealer3 = (int)(Math.random() * 10) + 2;
+                        dTotal += dealer3;
+                        System.out.println("");
+                        System.out.println("Dealer pulled a card: " + dealer3);
+                        System.out.println("Dealer's new total: " + dTotal);
+                    }
+
+                    if (dTotal > 21) {
+                        out = "";
+                        System.out.println("");
+                        System.out.println("House Bust!");
+                        System.out.println("You Win!");
+                        if (card1 + card2 == pTotal && pTotal == 21) {
+                            money += bet;
+                            bet = bet * 1.5;
+                            money +=bet;
+                            System.out.println("Blackjack!");
+                            System.out.println("Money: " + money);
+                        }
+                        else {
+                            money += bet;
+                            money += bet;
+                            System.out.println("Money: " + money);
+                        }
+                    }
+                    else if (pTotal > dTotal){
+                        out = "";
+                        System.out.println("");
+                        System.out.println("You Win!");
+                        if (card1 + card2 == pTotal && pTotal == 21) {
+                            money += bet;
+                            bet = bet * 1.5;
+                            money +=bet;
+                            System.out.println("Blackjack!");
+                            System.out.println("Money: " + money);
+                            continue;
+                        }
+                        else {
+                            money += bet;
+                            money += bet;
+                            System.out.println("Money: " + money);
+                            continue;
+                        }
+                    }
+                    else if (dTotal > pTotal){
+                        out = "";
+                        System.out.println("");
+                        System.out.println("House Wins!");
+                        System.out.println("Money: " + money);
+                        continue;
+                    }
+                    else if (dTotal == pTotal) {
+                        out = "";
+                        System.out.println("");
+                        System.out.println("It's a tie!");
+                        money += bet;
+                        System.out.println("Money: " + money);
+                        continue;
+                    }
+
+                    rep++;
                 }
                 
             }
 
 
-            System.out.println("Bet: ");
-            double bet = scanner.nextDouble();
-            money -= bet;
-
-
-            int card1 = (int)(Math.random() * 10) + 2;
-            int card2 = (int)(Math.random() * 10) + 2;
-            int pTotal = card1 + card2;
-
-            int dealer1 = (int)(Math.random() * 10) + 2;
-            int dealer2 = (int)(Math.random() * 10) + 2;
-            int dTotal = dealer1 + dealer2;
-
-            System.out.println("");
-            System.out.println("Dealer has: " + dealer1);
-            System.out.println("");
-            System.out.println("Your cards: " + card1 + " and " + card2);
-
-            String out = "";
-
-
-            while (pTotal < 22 && out.isEmpty()){
-                System.out.println("");
-                System.out.println("Hit, Stand, or Double Down?");
-                String choice = scanner.next();
-                choice = choice.toLowerCase();
-
-                if (choice.equals("hit")) {
-                    int card = (int)(Math.random() * 10) + 2;
-                    pTotal += card;
-                    System.out.println("");
-                    System.out.println("You were dealt: " + card);
-                    System.out.println("Total: " + pTotal);
-                }
-                else if (choice.equals("stand")) {
-                    out = "stand";
-                }
-                else if (true) {
-                    out = "stand";
-                    money -= bet;
-                    bet = bet*2;
-                    int card = (int)(Math.random() * 10) + 2;
-                    pTotal += card;
-                    System.out.println("");
-                    System.out.println("You were dealt: " + card);
-                    System.out.println("Total: " + pTotal);
-                }
-            }
-
-            if (pTotal >= 22){
-                out = "";
-                System.out.println("");
-                System.out.println("You bust!");
-                System.out.println("Money: " + money);
-            }
-
-            while (dTotal < 17 && pTotal < 22) {
-                int dealer3 = (int)(Math.random() * 10) + 2;
-                dTotal += dealer3;
-                System.out.println("");
-                System.out.println("Dealer pulled a card: " + dealer3);
-                System.out.println("Dealer's new total: " + dTotal);
-            }
-
-            if (dTotal > 21) {
-                out = "";
-                System.out.println("");
-                System.out.println("House Bust!");
-                System.out.println("You Win!");
-                if (card1 + card2 == pTotal && pTotal == 21) {
-                    money += bet;
-                    bet = bet * 1.5;
-                    money +=bet;
-                    System.out.println("Blackjack!");
-                    System.out.println("Money: " + money);
-                }
-                else {
-                    money += bet;
-                    money += bet;
-                    System.out.println("Money: " + money);
-                }
-            }
-            else if (pTotal > dTotal){
-                out = "";
-                System.out.println("");
-                System.out.println("You Win!");
-                if (card1 + card2 == pTotal && pTotal == 21) {
-                    money += bet;
-                    bet = bet * 1.5;
-                    money +=bet;
-                    System.out.println("Blackjack!");
-                    System.out.println("Money: " + money);
-                }
-                else {
-                    money += bet;
-                    money += bet;
-                    System.out.println("Money: " + money);
-                }
-            }
-            else if (dTotal > pTotal){
-                out = "";
-                System.out.println("");
-                System.out.println("House Wins!");
-                System.out.println("Money: " + money);
-            }
-            else if (dTotal == pTotal) {
-                out = "";
-                System.out.println("");
-                System.out.println("It's a tie!");
-                money += bet;
-                System.out.println("Money: " + money);
-            }
-
-            rep++;
+            
         }
         
 
