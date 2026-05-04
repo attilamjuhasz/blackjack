43cb98df780a902732380cf9399b2d7480d1ca46
Attila Juhasz <106558542+attilamjuhasz@users.noreply.github.com>
2026-05-04 16:30:42 +0000

    Working

---
+++ main.py
@@ -0,0 +1,136 @@
+import random
+
+play = False
+
+money = 0
+bet = 0
+
+print("Welcome to Attila's Blackjack")
+play = input("Do you want to play (y or n): ")
+if play == "y":
+    play = True
+    money = int(input("How much do you want to put in?: "))
+else:
+    play = False
+
+def getCards():
+    one = random.randint(2, 14)
+    two = random.randint(2, 14)
+    return one, two
+
+def getOneCard():
+    one = random.randint(2, 14)
+    return one
+
+def highCard(card):
+    if card == 11:
+        return "Jack"
+    elif card == 12:
+        return "Queen"
+    elif card == 13:
+        return "King"
+    elif card == 14:
+        return "Ace"
+    else:
+        return card
+
+
+while play:
+    bet = int(input("How much do you want to bet?: "))
+    while bet <= 0 or money-bet < 0:
+        bet = int(input("How much do you want to bet?: "))
+    blackJack = False
+    print("Shuffling deck...")
+
+    valOne, valTwo = getCards()
+    cardOne = highCard(valOne)
+    cardTwo = highCard(valTwo)
+
+    if valOne > 10 and valOne != 14:
+        valOne = 10
+    elif valOne == 14:
+        valOne = 11
+    
+    if valTwo > 10 and valTwo != 14:
+        valTwo = 10
+    elif valTwo == 14 and valOne == 11:
+        valTwo = 1
+    elif valTwo == 14:
+        valTwo = 11
+    
+    total = valOne + valTwo
+
+    dValOne, dValTwo = getCards()
+    dCardOne = highCard(dValOne)
+    dCardTwo = highCard(dValTwo)
+
+    if dValOne > 10 and dValOne != 14:
+        dValOne = 10
+    elif dValOne == 14:
+        valOne = 11
+    if dValTwo > 10 and dValTwo != 14:
+        dValTwo = 10
+    elif dValTwo == 14 and dValOne == 11:
+        valTwo = 1
+    elif dValTwo == 14:
+        valTwo = 11
+    dTotal = dValOne + dValTwo
+
+    print()
+    print(f"Dealer has a {dCardOne}")
+    print("You have a " + str(cardOne) + " and a " + str(cardTwo))
+    if total == 21:
+        "Blackjack!"
+        blackJack = True
+
+    decision = True
+    while decision and not blackJack:
+        type = input("Hit, Check, or Double Down (h, c, d): ")
+
+        if type == "c":
+            decision = False
+        
+        elif type == "h":
+            newVal = getOneCard()
+            newCard = highCard(newVal)
+
+            if newVal > 10 and newVal != 14:
+                newVal = 10
+            elif newVal == 14:
+                newVal = 11
+            print("You have " + str(total) + " and a " + str(newCard))
+            total += newVal
+            if total > 21:
+                print("Bust!")
+                money -= bet
+                decision = False
+            elif total == 21:
+                blackJack = True
+                print("Blackjack!")
+                decision = False
+            else:
+                decision = True
+        elif type == "d":
+            bet *= 2
+            newVal = getOneCard()
+            newCard = highCard(newVal)
+
+            if newVal > 10 and newVal !=14:
+                newVal = 10
+            elif newVal == 14 and total < 11:
+                newVal = 11
+            elif newVal == 14:
+                newVal = 1
+            print("You have " + str(total) + " and a " + str(newCard))
+            total += newVal
+            if total > 21:
+                print("Bust!")
+                money -= bet
+                decision = False
+            elif total == 21:
+                blackJack = True
+                print("Blackjack!")
+                decision = False
+            else:
+                decision = False
+        
